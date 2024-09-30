eac70fffb9b16c5d8ba6e4779685c16f6a46c27b
Angrybeef1 <136150862+Angrybeef1@users.noreply.github.com>
2024-09-29 19:49:18 -0700

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+// COMSC-210 | Lab 12 | Robert Stonemetz
+// using Visual Studios
+
+#include <iostream>
+
+using namespace std;
+
+
+int main () {
+    cout << "Hello World!" << endl;
+
+    return 0;
+}
